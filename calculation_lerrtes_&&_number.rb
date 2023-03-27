0aaf35ad68c478fb582e72fe9413417585eb3b26
ahmadi1401 <ahmadi1355esmaeel@gmail.com>
2023-03-27 12:52:23 +0430

    Calculation of the number of uppercase and lowercase letters and the number of a string

---
+++ calculation_lerrtes_&&_number.rb
@@ -0,0 +1,26 @@
+#برنامه‌ای بنویسید که یک رشته دریافت کند و تعداد حروف کوچک، بزرگ و اعداد در آن را محاسبه کند.
+
+Lowercase_letters = []
+uppercase_letters = []
+numbers = []
+
+print"Enter the letters :"
+user_letters = gets.chomp
+
+if user_letters == ''
+	puts"The input is invalid"
+else
+	array_letter = user_letters.split('')
+	
+	array_letter.each do |letter|
+		Lowercase_letters.push(letter) if ('a'..'z').include?(letter)
+		uppercase_letters.push(letter) if ('A'..'Z').include?(letter)
+		numbers.push(letter) if (1..9).include?(letter.to_i )
+	end
+	
+	puts "the lowercase letter = #{Lowercase_letters.length}"
+	puts "the uppercase letter = #{uppercase_letters.length}"
+	puts "the numbers  = #{numbers.length}"
+
+end
+
