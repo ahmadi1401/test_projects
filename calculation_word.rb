c7224bd11ecb7ea3629dbe9947ed3a959ddd020b
ahmadi1401 <ahmadi1355esmaeel@gmail.com>
2023-03-27 13:10:18 +0430

    Find the number of words in a string

---
+++ calculation_word.rb
@@ -0,0 +1,8 @@
+#رنامه‌ای بنویسید که یک رشته از کاربر دریافت کند و تعداد کلمات آن را محاسبه کند
+
+print "Enter your string:"
+user_input = gets.chomp
+
+user_convert = user_input.split(' ')
+
+puts user_convert.length
